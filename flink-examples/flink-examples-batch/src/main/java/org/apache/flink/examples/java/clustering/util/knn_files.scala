e251d89da5e3704a19efa06dd591a250e8705c08
macintario <macintario@gmail.com>
2018-12-06 14:14:01 -0600

    Eliminado "for"  y agregado FinalPoint(categoria,distancia,w,x,y,z)

--- flink-examples/flink-examples-batch/src/main/java/org/apache/flink/examples/java/clustering/util/knn_files.scala
+++ flink-examples/flink-examples-batch/src/main/java/org/apache/flink/examples/java/clustering/util/knn_files.scala
@@ -6,6 +6,7 @@ import org.apache.flink.api.scala
 import org.apache.flink.api.scala._
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.api.common.operators.Order._
+import org.apache.flink.api.java.DataSet
 
 
 
@@ -14,13 +15,14 @@ import org.apache.flink.api.common.operators.Order._
 object knn_files {
   case class Point(w: Double, x: Double, y:Double, z:Double, label: String)
   case class NewPoint(label: String, distance: Double)
+  case class FinalPoint(label: String, distance: Double, w: Double, x: Double, y:Double, z:Double)
   case class PointValidate(w: Double, x: Double, y:Double, z:Double)
 
   def main(args: Array[String]): Unit = {
     val params: ParameterTool = ParameterTool.fromArgs(args)
     val env = ExecutionEnvironment.getExecutionEnvironment
     env.getConfig.setGlobalJobParameters(params)
-    val k = 6
+    val k = 3
     val pointsTrain: scala.DataSet[Point] =
       env.readCsvFile(
         "/git/macintario_at_gmail/Diplomado Big Data/Modulo 4/flinka/flink(1)/flink-master/flink-examples/flink-examples-batch/src/main/java/org/apache/flink/examples/java/clustering/util/iris.csv",
@@ -29,27 +31,33 @@ object knn_files {
     val pointsClas : scala.DataSet[PointValidate] = env.readCsvFile(
       "/git/macintario_at_gmail/Diplomado Big Data/Modulo 4/flinka/flink(1)/flink-master/flink-examples/flink-examples-batch/src/main/java/org/apache/flink/examples/java/clustering/util/validate.csv"
     )
-
-    val puntos = pointsClas.collect()
-    for (dot <- puntos){
-      val train = pointsTrain
-      val puntofinal = train.map(x => NewPoint(x.label,
-        math.sqrt(
-            (dot.x-x.x)*(dot.x-x.x)
-              +(dot.y-x.y)*(dot.y-x.y)
-              +(dot.w-x.w)*(dot.w-x.w)
-              +(dot.z-x.z)*(dot.z-x.z)
-          )))
-        .sortPartition(1,order = ASCENDING)
-        .first(k)
-        .map(x  => NewPoint(x.label,1))
-        .groupBy(0)
-        .sum(1)
-        .sortPartition(1,DESCENDING)
-        .first(1)
-
-    }
-    println(salida.length)
+    val clasificados = pointsClas.collect().map(x => asignaEtiqueta(x, pointsTrain, k))
+    print("k=")
+    println(k)
+    println("Resultados")
+    println(clasificados)
+    //clasificados.
+  }
+  def asignaEtiqueta  (pV: PointValidate, pointsTrain: scala.DataSet[Point], k:Int) ={
+    val train = pointsTrain
+    var puntofinal = train.map(x => NewPoint(x.label,
+      math.sqrt(
+        (pV.x-x.x)*(pV.x-x.x)
+          +(pV.y-x.y)*(pV.y-x.y)
+          +(pV.w-x.w)*(pV.w-x.w)
+          +(pV.z-x.z)*(pV.z-x.z)
+      )))
+      .sortPartition(1,order = ASCENDING)
+      .first(k)
+      .map(x  => NewPoint(x.label,1))
+      .groupBy(0)
+      .sum(1)
+      .sortPartition(1,DESCENDING)
+      .first(1)
+//      .map(x=>Point(pV.w,pV.x,pV.y,pV.z,x.label))
+      .map(x=>FinalPoint(x.label, x.distance,pV.w,pV.x,pV.y,pV.z))
+    puntofinal.collect().toString()
   }
 
 }
+
